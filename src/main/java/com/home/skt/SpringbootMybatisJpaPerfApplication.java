313b844dcfc362918e6bbd88cea1c561e05a2b69
spmqjl <spmjql@example.com>
2024-03-05 22:14:16 +0900

    create repo

---
+++ src/main/java/com/home/skt/SpringbootMybatisJpaPerfApplication.java
@@ -0,0 +1,13 @@
+package com.home.skt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisJpaPerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisJpaPerfApplication.class, args);
+	}
+
+}
