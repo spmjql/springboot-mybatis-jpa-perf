47cb9dc54f70a3f05de834b320e8ebddfe6d099b
spmjql <141596791+spmjql@users.noreply.github.com>
2024-03-06 23:55:53 +0900

    Revert "Revert "create repo""
    This reverts commit 322139def6efe029ec33c29eab384a778e7167ad.

---
+++ src/test/java/com/home/skt/SpringbootMybatisJpaPerfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.skt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisJpaPerfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
