8d37ee294ae792701df7b24fd8a1010bde8dcf7e
spmqjl <spmjql@example.com>
2024-03-06 23:21:02 +0900

    게시글 데이터 3만개 입력

---
+++ src/main/java/com/home/skt/domain/entity/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.home.skt.domain.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long>{
+
+}
