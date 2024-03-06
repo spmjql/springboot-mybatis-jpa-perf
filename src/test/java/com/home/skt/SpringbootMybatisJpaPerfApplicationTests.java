02a8ef8d896c5927a138948d925719f274220465
spmjql <141596791+spmjql@users.noreply.github.com>
2024-03-06 23:39:40 +0900

    Revert "게시글 데이터 3만개 입력"
    This reverts commit 8d37ee294ae792701df7b24fd8a1010bde8dcf7e.

--- src/test/java/com/home/skt/SpringbootMybatisJpaPerfApplicationTests.java
+++ src/test/java/com/home/skt/SpringbootMybatisJpaPerfApplicationTests.java
@@ -1,34 +1,13 @@
 package com.home.skt;
 
-import java.util.stream.IntStream;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.home.skt.domain.dto.PostSave;
-import com.home.skt.domain.entity.BoardRepository;
 
 @SpringBootTest
 class SpringbootMybatisJpaPerfApplicationTests {
 
-	/*
 	@Test
 	void contextLoads() {
-	}
-	*/
-	
-	@Autowired
-	private BoardRepository boardRepository;
-	//@Test
-	void savePostTest() {
-		IntStream.rangeClosed(0, 100000).forEach(i->{
-			boardRepository.save(PostSave.builder()
-					.title("게시글 제목"+i)
-					.content("게시글 내용"+i)
-					.writer("작성자"+i)
-					.build().toEntity());
-		});
 	}
 
 }
